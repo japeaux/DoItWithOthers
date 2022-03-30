997b37ca933887d262b8960296d4d0e072442894
japeaux <lucascovatti@hotmail.com>
2022-03-30 13:37:10 -0300

    first commit

---
+++ www/lib/aoth/config.js
@@ -0,0 +1,8 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_nOWA67ADf', // e.g. us-east-2_uXboG5pAb
+        region: 'us-east-2', // e.g. us-east-2
+		clientId: '8k5rbqhifi5r1hb2j4n1u07li' //is this used anywhere?
+    },
+};
+
